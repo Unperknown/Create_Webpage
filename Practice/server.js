0c838feeb1c3f961915f3f1ea1074337a593e133
unKNOWn <37747694+Unperknown@users.noreply.github.com>
2018-07-14 16:27:37 +0900

    Code is a language.

--- Practice/server.js
+++ Practice/server.js
@@ -6,7 +6,15 @@ http.createServer(function (req, res) {
   console.log(req.url);
   var qs = url.parse(req.url, true).query;
   console.log(qs.id);
-  fs.readFile(`./Data/${qs.id}.html`, 'utf8', function(error, data){
+  fs.readdir(`./Data`, function(error, files) {
+    var i = 0;
+    var list = '<ol>';
+    while (i < files.length) {
+      list += `<li><a href=/?id=${files[i]}> ${files[i]}</a></li>`;
+      i++;
+    }
+    list += '<ol>';
+    fs.readFile(`./Data/${qs.id}`, 'utf8', function(error, data){
     res.write(`
       <!DOCTYPE html>
       <html lang="en" dir="ltr">
@@ -19,22 +27,16 @@ http.createServer(function (req, res) {
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"
           </script>
+          <script src="tmp.js"></script>
         </head>
         <body>
-          <script src="tmp.js"></script>
           <h1><a href="/?id=WEB">WEB</a></h1>
           <div class="grid">
             <div>
-              <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-                <li><a href="/?id=Node_js">Node_js</a></li>
-              </ol>
+              ${list}
             </div>
             <div class="line">
               <input class="button" type="button" name="Button" value="Default" onclick="colorToggle();">
-              <input class="luck" type="button" value="Show" onclick="showLuckyNum();">
               <h2>${qs.id}</h2>
               <!--jQuery를 사용하여 URL의 id 불러오기-->
               <p>${data}</p>
@@ -45,5 +47,6 @@ http.createServer(function (req, res) {
     console.log(data);
     //write a response to the client
     res.end(); //end the response
+    });
   });
 }).listen(8080); //the server object listens on port 8080:1337/');
